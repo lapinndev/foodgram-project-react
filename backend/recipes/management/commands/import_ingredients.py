fb34a41c518a68aa71ecc06e7a18171b4570542d
unknown <lapindev@yandex.ru>
2023-08-06 19:59:57 +0300

    final

---
+++ backend/recipes/management/commands/import_ingredients.py
@@ -0,0 +1,19 @@
+import json
+
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('/foodgram', type=str)
+
+    def handle(self, *args, **options):
+        with open(options['/foodgram'], encoding="utf8") as f:
+            data = json.load(f)
+        for item in data:
+            Ingredient.objects.create(
+                name=item['name'],
+                measurement_unit=item['measurement_unit']
+            )
